189779af6ec2cf6c94c2094e9dccd23dcbc272a7
Mr.-x- <mistax2.0z@gmail.com>
2024-09-24 15:13:10 +0530

    Add files via upload

---
+++ pingmasterX.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo -e "\033[1;34m <..Installing PingmasterX..>  \033[0m"
+
+termux-setup-storage
+pkg install -y wget && pkg install -y python
+pip install tqdm
+pip install ping3
+
+wget --no-check-certificate 'wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1N1aCysYcErEUXJfdtRTgNEkNYJ4G1hLI' -O PingmasterX' -O PingmasterX 
+
+chmod +x PingmasterX 
+mv PingmasterX $PREFIX/bin/PingmasterX
+
+
+echo -e "\033[1;32mScript executed successfully \033[0m"
+echo -e "\033[1;33mType PingmasterX to run \033[0m"
